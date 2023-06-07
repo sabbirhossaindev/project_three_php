7e5c45a01b3b8ccf00de13cb78fc5a8fa22c171d
sabbir hossain <mdsabbir477470@gmail.com>
2023-06-07 12:21:49 +0600

    from done checked

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+header('X-XSS-protection:0');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,22 +18,35 @@
     }
 </style>
 <body>
-<div class="container">
+    <div class="container">
         <div class="row">
             <div class="column column-60 column-offset-20">
                 <h2>Our Fist From</h2>
-                <hr class="hr">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae et tempore voluptatibus veniam porro assumenda quos eaque reiciendis, fugiat, nulla, debitis id minus voluptatum commodi possimus. Earum voluptas quasi inventore consequatur odit architecto, eveniet amet!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae et tempore voluptatibus veniam porro assumenda quos eaque reiciendis, fugiat, nulla!</p>
+                <p>
+                    <?php if(isset($_POST['fname'])&& !empty($_POST['fname'])) {?>
+                       First Name:  <?php echo $_POST['fname']?> <br />
+                    <?php } ?>
+
+                    <?php if(isset($_POST['lname'])&& !empty($_POST['lname'])){ ?>
+                       Last Name:  <?php echo $_POST['lname']?> <br />
+                    <?php } ?>
+                </p>
             </div>
         </div>
         <div class="row">
             <div class="column column-60 column-offset-20">
-                <form action="">
+                <form action="POST">
                     <label for="fname">First Name</label>
                     <input type="text" name="fname" id="fname">
                     
                     <label for="lname">Last Name</label>
                     <input type="text" name="lname" id="lname">
+
+                    <div>
+                        <input type="checkbox" name="cb1" id="cb1">
+                        <label for="cb1" class="label-inline">Some checkbox</label>
+                    </div>
 
                     <button type="submit">Submit</button>
                 </form>
